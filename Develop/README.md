39dbf9548fb4d4cb3665b5aec91620ea09369529
Kevin S <kev_ace04@yahoo.com>
2021-02-18 18:55:03 -0500

    initial commit, start README

---
+++ Develop/README.md
@@ -0,0 +1,25 @@
+# readme
+
+## Description
+
+a readme file
+
+## Installation
+
+read the readme
+
+## Usage
+
+keep reading me
+
+## Credit
+
+someone else read me
+
+## License
+
+none
+
+## Tests
+
+just keep reading me
---
+++ Develop/index.js
@@ -0,0 +1,102 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        name: "title",
+        type: "input",
+        message: "What is the title?",
+    },
+    {
+        name: "description",
+        type: "input",
+        message: "What is the description?",
+    },
+    {
+        name: "install",
+        type: "input",
+        message: "What are the installation instructions?",
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "What is the usage information?",
+    },
+    {
+        name: "contribution",
+        type: "input",
+        message: "What are the contribution guidelines?",
+    },
+    {
+        name: "test",
+        type: "input",
+        message: "What are the test instructions?",
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "Do you want to include a license?",
+        choices: ["BSD", "MIT", "GPL", "none"],
+    },
+    {
+        name: "username",
+        type: "input",
+        message: "What is my GitHub username?",
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "What is my email address?",
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    const content = data.title + data.description + data.installation + data.usage + data.credits + data.license + data.test
+    fs.writeFile(fileName, content, (err) => {
+        if (err) throw err;
+    
+        console.log("The file was succesfully saved!");
+    }); 
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        const data = {
+            title: `# ${answers.title}\n\n`,
+            description: `## Description\n\n ${answers.description}\n\n`,
+            installation: `## Installation\n\n ${answers.install}\n\n`,
+            usage: `## Usage\n\n ${answers.usage}\n\n`,
+            credits: `## Credit\n\n ${answers.contribution}\n\n`,
+            license: `## License\n\n ${answers.license}\n\n`,
+            test: `## Tests\n\n ${answers.test}\n\n`,
+            username: answers.username,
+            email: answers.email,
+        }
+        writeToFile("README.md", data)
+    })
+}
+
+// Function call to initialize app
+init();
